8fc8c002c623d2dd30212adb315412b961dd4fc8
Wojtek <4golda4@gmail.com>
2019-05-11 12:15:06 +0200

    final final

--- app/src/main/java/com/example/wielkagra/MainActivity.java
+++ app/src/main/java/com/example/wielkagra/MainActivity.java
@@ -22,7 +22,6 @@ public class MainActivity extends AppCompatActivity {
 
 
     int punkty=0;
-    int d=0;
     int mss=0;
 
     boolean gra;
@@ -39,17 +38,12 @@ public class MainActivity extends AppCompatActivity {
             R.drawable.wzor_trzy,
             R.drawable.trygrys_kopia};
     ArrayList<Integer> lista = new ArrayList<Integer>();
-    ArrayList<Integer> lista2 = new ArrayList<Integer>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         gra=true;
-        
-
-
-
-
         final Handler handler2 = new Handler();
         handler2.post(new Runnable() {
             @Override
@@ -60,13 +54,6 @@ public class MainActivity extends AppCompatActivity {
                 handler2.postDelayed(this,1000);
             }
         });
-
-
-
-
-
-
-
 
         Random rand= new Random();
 
@@ -85,26 +72,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-
-               // Toast.makeText(getApplicationContext(),images[2],Toast.LENGTH_LONG).show();
-
-
-               lista2.add(lista.get(0));
-             //   Toast.makeText(getApplicationContext(),images[2],Toast.LENGTH_LONG).show();
-                d++;
                 przycisk1.setImageResource(lista.get(0));
-
-
-
-
-
-
-
-
-
-
                 final Handler handler = new Handler();
-
                 handler.post(new Runnable() {
                     boolean  run=true;
                     int mseconds1=0;
@@ -144,8 +113,6 @@ public class MainActivity extends AppCompatActivity {
                                         punkty++;
                                     }
 
-
-
                                 }
                                 else{
                                     poprzednio=lista.get(0);
@@ -168,21 +135,15 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
 
 
-                d++;
-               lista2.add(lista.get(1));
+
                 przycisk2.setImageResource(lista.get(1));
                 final Handler handler = new Handler();
-
-
-
-
 
                 handler.post(new Runnable() {
                     boolean  run=true;
                     int mseconds2=0;
                     @Override
                     public void run() {
-
 
 
                         if(run)
@@ -242,22 +203,16 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-               lista2.add(lista.get(2));
-                d++;
+
+
                 przycisk3.setImageResource(lista.get(2));
                 final Handler handler = new Handler();
-
-
-
-
 
                 handler.post(new Runnable() {
                     boolean  run=true;
                     int mseconds3=0;
                     @Override
                     public void run() {
-
-
 
                         if(run)
                         {
@@ -318,20 +273,16 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-               lista2.add(lista.get(3));
-                d++;
+
+
                 przycisk4.setImageResource(lista.get(3));
                 final Handler handler = new Handler();
-
-
-
 
                 handler.post(new Runnable() {
                     boolean  run=true;
                     int mseconds4=0;
                     @Override
                     public void run() {
-
 
                         if(run)
                         {
@@ -367,7 +318,6 @@ public class MainActivity extends AppCompatActivity {
                                         punkty++;
                                     }
 
-
                                 }
                                 else{
                                     poprzednio=lista.get(3);
@@ -391,15 +341,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                lista2.add(lista.get(4));
-                d++;
 
                 final Handler handler = new Handler();
                 przycisk5.setImageResource(lista.get(4));
-
-
-
-
 
                 handler.post(new Runnable() {
                     boolean  run=true;
@@ -420,8 +364,6 @@ public class MainActivity extends AppCompatActivity {
                                 )
                                 {
 
-
-
                                     przycisk5.setImageResource(R.drawable.okejka_tak);
                                     przycisk5.setVisibility(View.INVISIBLE);
                                     ImageButton przycisk5 = (ImageButton)findViewById(przycisk);
@@ -441,13 +383,11 @@ public class MainActivity extends AppCompatActivity {
                                         punkty++;
                                     }
 
-
                                 }
                                 else{
                                     poprzednio=lista.get(4);
                                     przycisk=R.id.but5;
                                     przycisk5.setImageResource(R.drawable.poczatkowy);
-
                                 }
                             }
                         }
@@ -462,14 +402,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-               lista2.add(lista.get(5));
-                d++;
                 przycisk6.setImageResource(lista.get(5));
                 final Handler handler = new Handler();
-
-
-
-
                 handler.post(new Runnable() {
                     boolean  run=true;
                     int mseconds6=0;
@@ -488,7 +422,6 @@ public class MainActivity extends AppCompatActivity {
                                         (lista.get(5)==images[7]&&poprzednio==images[8])||(lista.get(5)==images[8]&&poprzednio==images[7])
                                 )
                                 {
-
 
                                     przycisk6.setImageResource(R.drawable.okejka_tak);
                                     przycisk6.setVisibility(View.INVISIBLE);
@@ -527,21 +460,15 @@ public class MainActivity extends AppCompatActivity {
         przycisk7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                lista2.add(lista.get(6));
-                d++;
+
                 przycisk7.setImageResource(lista.get(6));
                 final Handler handler = new Handler();
-
-
-
-
 
                 handler.post(new Runnable() {
                     boolean  run=true;
                     int mseconds7=0;
                     @Override
                     public void run() {
-
 
                         if(run)
                         {
@@ -585,7 +512,6 @@ public class MainActivity extends AppCompatActivity {
 
                                 }
 
-
                             }
                         }
                         handler.postDelayed(this,1000);
@@ -601,21 +527,16 @@ public class MainActivity extends AppCompatActivity {
         przycisk8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                lista2.add(lista.get(7));
-                d++;
+
+               
                 przycisk8.setImageResource(lista.get(7));
                 final Handler handler = new Handler();
-
-
-
-
 
                 handler.post(new Runnable() {
                     boolean  run=true;
                     int mseconds8=0;
                     @Override
                     public void run() {
-
 
                         if(run)
                         {
@@ -670,16 +591,11 @@ public class MainActivity extends AppCompatActivity {
         przycisk9.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                lista2.add(lista.get(8));
-                d++;
-
-
 
 
 
                 przycisk9.setImageResource(lista.get(8));
                 final Handler handler = new Handler();
-
 
                 handler.post(new Runnable() {
                     boolean  run=true;
@@ -702,16 +618,11 @@ public class MainActivity extends AppCompatActivity {
                                 )
                                 {
 
-
-
-
                                     przycisk9.setImageResource(R.drawable.okejka_tak);
                                     przycisk9.setVisibility(View.INVISIBLE);
                                     ImageButton przycisk9 = (ImageButton)findViewById(przycisk);
                                     przycisk9.setImageResource(R.drawable.okejka_tak);
                                     przycisk9.setVisibility(View.INVISIBLE);
-
-
 
                                     if(punkty==4)
                                     {
@@ -727,7 +638,6 @@ public class MainActivity extends AppCompatActivity {
                                         punkty++;
                                     }
 
-
                                 }
                                 else{
                                     poprzednio=lista.get(8);
@@ -735,55 +645,17 @@ public class MainActivity extends AppCompatActivity {
                                     przycisk9.setImageResource(R.drawable.poczatkowy);
                                 }
 
-
                             }
                         }
                         handler.postDelayed(this,1000);
                     }
 
                 });
-               // Toast.makeText(getApplicationContext(),Integer.toString(R.drawable.button_gra),Toast.LENGTH_SHORT).show();
 
             }
         });
 
-
-
-
-
-
-
-
-
-
-
-
-
     }
-
-
-
 
 }
 
-/*
-                for(int i=0;i<lista2.size();i++)
-                {
-                    if((lista2.get(i).equals(images[2]))&&(lista2.get(i-1).equals(images[3]))||(lista2.get(i-1).equals(images[2]))&&(lista2.get(i).equals(images[3])))
-                    {
-                        Toast.makeText(getApplicationContext(),"Dostajesz punkt",Toast.LENGTH_SHORT).show();
-                    }
-                    else if((lista2.get(i).equals(images[6]))&&(lista2.get(i-1).equals(images[5]))||(lista2.get(i-1).equals(images[6]))&&(lista2.get(i).equals(images[5])))
-                    {
-                        Toast.makeText(getApplicationContext(),"Dostajesz punkt",Toast.LENGTH_SHORT).show();
-                    }
-                    else if((lista2.get(i).equals(images[0]))&&(lista2.get(i-1).equals(images[1]))||(lista2.get(i-1).equals(images[0]))&&(lista2.get(i).equals(images[1])))
-                    {
-                        Toast.makeText(getApplicationContext(),"Dostajesz punkt",Toast.LENGTH_SHORT).show();
-                    }
-                    else if((lista2.get(i).equals(images[7]))&&(lista2.get(i-1).equals(images[8]))||(lista2.get(i-1).equals(images[7]))&&(lista2.get(i).equals(images[8])))
-                    {
-                        Toast.makeText(getApplicationContext(),"Dostajesz punkt",Toast.LENGTH_SHORT).show();
-                    }
-                }
- */
